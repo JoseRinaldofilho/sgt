a90ee955923b06206f0d805f2ffaac0082b6b931
JOSE RINALDO DE VASCONCELOS FIlho <jose.vasconcelos@basis.com.br>
2021-07-20 09:58:38 -0300

    teste commit

---
+++ src/main/java/br/com/basis/sgt3/Sgt3Application.java
@@ -0,0 +1,14 @@
+package br.com.basis.sgt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sgt3Application {
+
+	public static void main(String[] args) {
+		//inicio
+		SpringApplication.run(Sgt3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/basis/sgt3/Sgt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.basis.sgt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sgt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
