b6db57c7409e9828d727408df7431f862cb1ef16
JOSE RINALDO DE VASCONCELOS FIlho <jose.vasconcelos@basis.com.br>
2021-08-13 10:21:48 -0300

    commit  ajuste respo

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AccordionModule} from 'primeng/accordion';
+import {MenuItem} from 'primeng/api';                  //api
+
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
